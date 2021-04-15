2b4fcce7c591ae3dd14bdc3e8e2b0580f19dcc84
ahmudie <60074291+ahmudie@users.noreply.github.com>
2021-04-15 23:23:40 +0200

    playermovement has been added

--- BulletHellJam/BulletHellJam/Assets/Scripts/PlayerMovement.cs
+++ BulletHellJam/BulletHellJam/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ProcessInputs(); 
     }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY);
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+
 }
