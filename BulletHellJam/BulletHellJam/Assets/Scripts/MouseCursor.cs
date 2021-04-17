cc33ed2fefb49fe975fcf8dae0da73e49da7516c
PhyntomOne <61550895+PHYNTOM1@users.noreply.github.com>
2021-04-17 17:16:10 +0200

    working bullet pooling!! pog

---
+++ BulletHellJam/BulletHellJam/Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 _mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.gameObject.transform.position = new Vector3(_mPos.x, _mPos.y, this.transform.position.z);
+    }
+}
